c4956d83f01c8809d0279ed820d80d5f5fa35b82
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-02-18 21:11:02 -0500

    Add models Medication and MedicationHistory

---
+++ MedicationReminderApp/Models/MedicationHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicationReminderApp.Models
+{
+    public class MedicationHistory
+    {
+        public int Id { get; set; }
+        public int MedicationId { get; set; }
+        public DateTime TakenTime { get; set; }
+        public bool Taken { get; set; }
+    }
+}
